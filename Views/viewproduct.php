e1327da65ae646b43d47ee8244fb5947fd06b0c5
dieutrinh28 <trinhdo.work@gmail.com>
2022-10-22 23:53:54 +0700

    product-detail

--- Views/viewproduct.php
+++ Views/viewproduct.php
@@ -1,14 +1,66 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Furni3dots - Home</title>
+    <link rel="stylesheet" href="../do-an-web-php/Public/CSS/header.css">
+    <link rel="stylesheet" href="../do-an-web-php/Public/CSS/productdetail.css">
+    <link rel="stylesheet" href="../do-an-web-php/Public/CSS/footer.css">
+    <!-- link bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <!-- link owl carousel -->
+    <link rel="stylesheet" href="owlcarousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/assets/owl.theme.default.min.css">
+    <!-- link icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+    <!-- link font -->
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
 
 <body>
-    <h1>Product Info</h1>
+    <!-- header -->
     <?php
-    echo "<b>ID: </b>" . $product->getId() . "<br/>";
-    echo "<b>Name: </b>" . $product->getName() . "<br/>";
-    echo "<b>Description: </b>" . $product->getDescription() . "<br/>";
-    echo "Thêm vào giỏ hàng";
+    require_once("../do-an-web-php/Views/blocks/header.php");
     ?>
+    <!-- body -->
+    <div class="body global">
+        <div class="left">
+            <?php
+            echo "<div class='item-img'>
+                     <img src='../do-an-web-php/Assets/img/" . $product->getImage() . "'>
+                </div>"
+            ?>
+
+        </div>
+        <div class="right">
+            <div class="emojis">
+                <a href=""><i class="fas fa-angle-left"></i></a>
+                <?php
+                echo "<h1 id='pro_name'>" . $product->getName() . "</h1>";
+                ?>
+
+                <a href=""><i class="fas fa-angle-right"></i></i></a>
+            </div>
+            <div id='content_border'>
+                <?php
+                echo "<b>Description: </b>" . $product->getDescription() . "<br/>";
+                ?>
+            </div>
+        </div>
+    </div>
+
+    <!--Footer-->
+    <?php
+    require_once("../do-an-web-php/Views/blocks/footer.php");
+    ?>
+    <!-- link bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
