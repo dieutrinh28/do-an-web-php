d72d4031e223e26ccaf4b16bebf8dd312d28fa9f
dieutrinh28 <trinhdo.work@gmail.com>
2022-11-14 04:55:36 +0700

    edit-css

--- Views/users/viewproduct.php
+++ Views/users/viewproduct.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Furni3dots - Product Detail</title>
     <link rel="stylesheet" href="../do-an-web-php/Public/CSS/header.css">
-    <link rel="stylesheet" href="../do-an-web-php/Public/CSS/productdetail.css">
+    <link rel="stylesheet" href="../../Public/CSS/productdetail.css">
     <link rel="stylesheet" href="../do-an-web-php/Public/CSS/footer.css">
     <!-- link bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
